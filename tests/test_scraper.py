6bd7cd94e35036991d497ee1e20480cde54942e6
soji-omiwade <omiwade@gmail.com>
2019-12-14 02:32:49 -0600

    correct the failing testcase

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -9,7 +9,7 @@ changing functionality will cause things here to break.
 
 class MyTestCase(unittest.TestCase):
     def test_checkProtocol(self):
-        self.assertTrue(scraper.checkProtocol("github.com"))
+        self.assertFalse(scraper.checkProtocol("github.com"))
         # assert checkProtocol("http:/github.com") == False
         # assert checkProtocol("https://github.com") == True
         # assert checkProtocol("https:/github.com") == False
