8188f9a231800c29a10f5b3be2a2a38ba71b07eb
soji-omiwade <omiwade@gmail.com>
2019-12-14 01:58:40 -0600

    cleanup

---
+++ tests/test_scraper.py
@@ -0,0 +1,16 @@
+import unittest
+import re
+import scraper
+	
+"""
+this is a regression test. 
+changing functionality will cause things here to break. 
+"""
+
+class MyTestCase(unittest.TestCase):
+    def test_checkProtocol(self):
+        self.assertTrue(scraper.checkProtocol("github.com"))
+        # assert checkProtocol("http:/github.com") == False
+        # assert checkProtocol("https://github.com") == True
+        # assert checkProtocol("https:/github.com") == False
+
