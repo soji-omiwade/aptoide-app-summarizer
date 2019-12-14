b1e83a06f675b9a6deb44616098d4e60c7b6160d
soji-omiwade <omiwade@gmail.com>
2019-12-14 03:47:53 -0600

    bug fix in test-case

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -10,7 +10,7 @@ changing functionality will cause things here to break.
 class MyTestCase(unittest.TestCase):
 
 
-    def test_scraper.url_is_valid(self):
+    def test_url_is_valid(self):
         self.assertFalse(scraper.url_is_valid("github.com"))
         self.assertFalse(scraper.url_is_valid("http:/github.com"))
         self.assertTrue(scraper.url_is_valid("https://github.com"))
